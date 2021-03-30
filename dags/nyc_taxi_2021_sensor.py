89965778743b1818acf8f1ef665a6f97ed18b97e
adilkhash <adil.khashtamov@gmail.com>
2021-03-30 11:40:35 +0600

    changed poke interval to 24 hours

--- dags/nyc_taxi_2021_sensor.py
+++ dags/nyc_taxi_2021_sensor.py
@@ -24,7 +24,7 @@ with DAG(
         endpoint='yellow_tripdata_{{ execution_date.strftime("%Y-%m") }}.csv',
         http_conn_id='nyc_yellow_taxi_id',
         task_id='check_if_exists',
-        poke_interval=60 * 5,
+        poke_interval=60 * 60 * 24,  # every 24 hours
     )
 
     @task
